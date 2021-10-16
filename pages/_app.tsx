226b7072cddc08ead869bae6fdf4b40b1c8e77df
Alex Bennett <alexandergraybennett@gmail.com>
2021-10-16 12:04:22 -0600

    wip: add SSE to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
 import App, { AppContext, AppInitialProps, AppProps } from "next/app";
+import { Context, initialRender } from "../context/sse.context";
 
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Context>
+      <Component {...pageProps} />
+    </Context>
+  );
 }
 
 MyApp.getInitialProps = async (appContext: AppContext) => {
   const data: AppInitialProps = await App.getInitialProps(appContext);
 
+  const sse = await initialRender(appContext, data);
+
   const pageProps = {
     ...data.pageProps,
+    ...sse,
   };
 
   return pageProps;
