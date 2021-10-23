85c95bf4f68dffd8d0b5e1cbd8605956120e7bd7
Guillaume FORTAINE <guillaume.fortaine-ext@mousquetaires.com>
2021-10-23 18:04:59 +0200

    refactor(eslint): Disallows the use of require statements

--- context/sse.context.tsx
+++ context/sse.context.tsx
@@ -1,16 +1,17 @@
 import ReactDOMServer from "react-dom/server";
 import { AppContext } from "next/app";
 import { NextComponentType, NextPageContext } from "next";
-import { createServerContext } from "use-sse";
-const compressPayload = require('compress-json')
+import { createServerContext, createBroswerContext } from "use-sse";
+import * as compressPayload from 'compress-json';
+
 export const { ServerDataContext, resolveData } = createServerContext();
 
 function getOrCreate() {
   if (process.browser) {
-    //decompress the SSR string
+    // decompress the SSR string
     // window._initialDataContext = window.__NEXT_DATA__.props.sse
     window._initialDataContext = compressPayload.decompress(window.__NEXT_DATA__.props.sse);
-    return require("use-sse").createBroswerContext();
+    return createBroswerContext();
   }
   return ServerDataContext;
 }
