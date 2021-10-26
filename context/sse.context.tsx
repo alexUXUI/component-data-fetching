a83ca225bb8f52ed8ced4e0827c7aa7122e6ad3c
Guillaume FORTAINE <guillaume.fortaine-ext@mousquetaires.com>
2021-10-26 19:19:12 +0200

    Revert "Merge pull request #1 from alexUXUI/enable_json_compression"
    This reverts commit 281da40a68eacbb02c94df7c212a21e1ee26aa6f, reversing
    changes made to 0f05839fbdf98386366b437cd6e4120df5eb1ed8.

--- context/sse.context.tsx
+++ context/sse.context.tsx
@@ -2,14 +2,12 @@ import ReactDOMServer from "react-dom/server";
 import { AppContext } from "next/app";
 import { NextComponentType, NextPageContext } from "next";
 import { createServerContext } from "use-sse";
-const compressPayload = require('compress-json')
+
 export const { ServerDataContext, resolveData } = createServerContext();
 
 function getOrCreate() {
   if (process.browser) {
-    //decompress the SSR string
-    // window._initialDataContext = window.__NEXT_DATA__.props.sse
-    window._initialDataContext = compressPayload.decompress(window.__NEXT_DATA__.props.sse);
+    window._initialDataContext = window.__NEXT_DATA__.props.sse;
     return require("use-sse").createBroswerContext();
   }
   return ServerDataContext;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,21 +13,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
 MyApp.getInitialProps = async (appContext: AppContext) => {
   const data: AppInitialProps = await App.getInitialProps(appContext);
-    const compressPayload = require('compress-json')
+
   const sse = await initialRender(appContext, data);
-  const compressedSSEData = compressPayload.compress(sse.data)
-  const b = JSON.stringify(sse.data).length * 2;
-  const kb = (b / 1024).toFixed(2);
-
-
-  const c = JSON.stringify(compressedSSEData).length * 2;
-  const kb1 = (c / 1024).toFixed(2);
-  console.log('compressed',kb1,'kb')
-  console.log('raw',kb,'kb')
 
   const pageProps = {
     ...data,
-    sse: compressedSSEData,
+    sse,
   };
+
   return pageProps;
 };
